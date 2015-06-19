d8a5c37b566ff815add1e5bea51d10db3047491b
Māris Krivtežs <marisks@gmail.com>
2015-06-19 12:59:08 +0300

    updated xunit

--- Geta.EPi.Commerce.Payments.Klarna.Checkout/Tests.Integration/TestConfig.cs
+++ Geta.EPi.Commerce.Payments.Klarna.Checkout/Tests.Integration/TestConfig.cs
@@ -5,7 +5,7 @@ namespace Tests.Integration
     public static class TestConfig
     {
         public static readonly Uri OrderBaseUri = new Uri("https://checkout.testdrive.klarna.com/checkout/orders");
-        public const string MerchantId = "xxx";
-        public const string SharedSecret = "xxx";
+        public const string MerchantId = "2943";
+        public const string SharedSecret = "mnvCDwuWjiLG2no";
     }
 }
