b4558406ae8a9576928c0c95df22b8cac95da362
Māris Krivtežs <marisks@gmail.com>
2015-06-19 15:43:01 +0300

    Fix for invalid discount rate

--- Geta.EPi.Commerce.Payments.Klarna.Checkout/Geta.EPi.Commerce.Payments.Klarna.Checkout/Extensions/LineItemExtensions.cs
+++ Geta.EPi.Commerce.Payments.Klarna.Checkout/Geta.EPi.Commerce.Payments.Klarna.Checkout/Extensions/LineItemExtensions.cs
@@ -8,12 +8,15 @@ namespace Geta.EPi.Commerce.Payments.Klarna.Checkout.Extensions
     {
         public static ICartItem ToCartItem(this LineItem lineItem, decimal taxRate = 0)
         {
+            var discountPerItem = lineItem.LineItemDiscountAmount/lineItem.Quantity;
+            var discountRate = discountPerItem*100/lineItem.PlacedPrice;
+
             return new CartItem(
                 lineItem.CatalogEntryId,
                 lineItem.DisplayName,
                 Convert.ToInt32(lineItem.Quantity),
                 Convert.ToInt32(lineItem.PlacedPrice * 100),
-                Convert.ToInt32(lineItem.LineItemDiscountAmount * 100),
+                Convert.ToInt32(discountRate * 100),
                 Convert.ToInt32(taxRate * 100));
         }
     }
