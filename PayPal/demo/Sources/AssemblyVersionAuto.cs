f1b9ce8dd95c3e0d3d41346bc21d5b2081cf6779
Fredrik Eckmar <fredrik.eckmar@gmail.com>
2017-05-05 10:12:23 +0200

    Removed old demo

--- PayPal/demo/Sources/AssemblyVersionAuto.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-
-
-
-[assembly: AssemblyVersion("10.2.3.0")]
-[assembly: AssemblyInformationalVersion("10.2.3")]
-[assembly: AssemblyFileVersion("10.2.3.2130")]
--- PayPal/demo/Sources/EPiServer.Reference.Commerce.Site/Features/Profile/ViewModels/ProfilePageViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using EPiServer.Reference.Commerce.Site.Features.Profile.Pages;
-using EPiServer.Reference.Commerce.Site.Features.Shared.ViewModels;
-
-namespace EPiServer.Reference.Commerce.Site.Features.Profile.ViewModels
-{
-    public class ProfilePageViewModel : PageViewModel<ProfilePage>
-    {
-    }
-}
